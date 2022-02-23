6375ffd7ac3c7c787602e11617b541fd9bf70880
Retr0 <mathewsloban@yandex.com>
2022-02-23 13:35:30 +0300

    Create parse.py

---
+++ parse.py
@@ -0,0 +1,21 @@
+
+import os
+
+if __name__ == '__main__':
+    print("================================================================")
+    print("=                     Begin parse file                         =")
+    print("================================================================")
+
+    list_files = os.listdir()
+    name = ''
+
+    for file in list_files:
+        name = file[:-7]
+        if os.path.isfile(file) and (file[file.__len__() - 7:]) == ".tar.gz":
+            if not os.path.isdir(name):
+                os.mkdir(name)
+                os.replace(file, name + "/" + file)
+
+    print("================================================================")
+    print("=                   Successful parse file                  =")
+    print("================================================================")
